502360760df7a048bc1db1b4da021d0afcdcd4af
DillonBorden <dillondaughtry2012@gmail.com>
2024-01-08 17:20:30 -0600

    lab complete

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
-// Write your code here
+const num1 = 31;
+const num2 = 2;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 10;
+let num4 = 6;
+
+let mod = num3 % num4;
+
+let max = Math.max(1,3,4,20);
